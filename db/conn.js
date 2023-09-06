2e99b1c535a57440e25abdfc15cda35b2a566ed2
pawan birnale <birnalepavan@gmail.com>
2023-09-06 20:37:09 +0530

    database connected to the atlas remote and changes in app.js using async/await

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-mongoose.connect("mongodb://0.0.0.0:27017/students-api").then(()=>{
+mongoose.connect("mongodb+srv://birnalepavan:12345@cluster0.yj2hdzw.mongodb.net/REST-APIretryWrites=true&w=majority").then(()=>{
     console.log("connection is successful");
 }).catch((e)=>{
 console.log(`${e}: NO connection establish`);
--- src/app.js
+++ src/app.js
@@ -4,19 +4,38 @@ const student = require("../models/student")
 const app = express();
 app.use(express.json())
 const port = process.env.PORT || 3000;
+
+// *******************using promises start
+
 // 
-app.post('/students',(req,res)=>{
-    console.log(req.body);
-    const user = new student(req.body);
-    user.save().then(()=>{
-       res.status(201).send(user); 
-    }).catch((e)=>{
+// app.post('/students',(req,res)=>{
+//     console.log(req.body);
+//     const user = new student(req.body);
+//     user.save().then(()=>{
+//        res.status(201).send(user); 
+//     }).catch((e)=>{
+//         res.status(400).send(e);
+//        console.log(`${e}: something went wrong`);
+//     })
+// })
+
+// *******************using promises end
+
+//*****************using async await start**********
+
+app.post('/students',async(req,res)=>{
+      try {
+        console.log(req.body);
+        const user = new student(req.body);
+      const createUser = user.save();
+      res.status(201).send(user);
+      } catch (e) {
         res.status(400).send(e);
-       console.log(`${e}: something went wrong`);
-    })
- 
+        console.log(`${e}: something went wrong`);
+      }
 })
 
+//*****************using async await end**********
 
 
 app.listen(port,()=>{
